cff9515c71d108fd326e2845636ef60c039ee3a8
Andrew Conrad <aconrad103@gmail.com>
2017-05-27 07:18:44 -0500

    simple_container: Adjust error message

--- service/simple_container.go
+++ service/simple_container.go
@@ -10,7 +10,7 @@ func (sc *SimpleContainer) Get(name string) (interface{}, error) {
 	if s, ok := sc.services[name]; ok {
 		return s, nil
 	}
-	return nil, fmt.Errorf("service: Container does not have service %q", name)
+	return nil, fmt.Errorf("service: SimpleContainer does not have service %q", name)
 }
 
 func (sc *SimpleContainer) Has(name string) bool {
