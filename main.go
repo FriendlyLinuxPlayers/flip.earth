5b9bacd21f53b0da8c7c08572a15657fc9a61d70
Andrew Conrad <aconrad103@gmail.com>
2017-05-27 15:40:46 -0500

    Move config parsing from main.go to service/config/reader.go
    main now uses a Builder, to get a Container, to get the Config and print
    it.

--- main.go
+++ main.go
@@ -1,63 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 
-	"github.com/friendlylinuxplayers/flip.earth/config"
+	_ "github.com/friendlylinuxplayers/flip.earth/config"
 	_ "github.com/friendlylinuxplayers/flip.earth/router"
 	_ "github.com/friendlylinuxplayers/flip.earth/server"
-	_ "github.com/friendlylinuxplayers/flip.earth/service"
+	"github.com/friendlylinuxplayers/flip.earth/service"
+	"github.com/friendlylinuxplayers/flip.earth/service/config"
 )
 
-// TODO refactor out everything so main only contains main func with minimal code
+// TODO refactor out everything so main only contains minimal code
 func main() {
-	cfg, err := parseConfig()
-
-	if err != nil {
-		panic(err)
+	b := new(service.Builder)
+	configDef := service.Definition{"config", make([]string, 0), make(map[string]interface{}), cservice.Reader{}}
+	b.Insert(configDef)
+	container, error := b.Build()
+	if error != nil {
+		panic(error)
+		return
 	}
 
-	cfgVal := *cfg
-
-	fmt.Printf("Config %+v \n", cfgVal)
-}
-
-func parseConfig() (*config.Config, error) {
-	cfgFile, err := ioutil.ReadFile("config/default/config.json")
-	if err != nil {
-		return nil, fmt.Errorf("error reading default config file: %s", err.Error())
+	service, error := container.Get("config")
+	if error != nil {
+		panic(error)
+		return
 	}
-	cfg := new(config.Config)
-	err = json.Unmarshal(cfgFile, cfg)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing default config file: %s", err.Error())
-	}
-
-	err = mergeUserConfig(cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	return cfg, nil
-}
-
-func mergeUserConfig(cfg *config.Config) error {
-	if _, err := os.Stat("config/config.json"); os.IsNotExist(err) {
-		return nil
-	}
-
-	userCfgFile, err := ioutil.ReadFile("config/config.json")
-	if err != nil {
-		return fmt.Errorf("error reading user config file: %s", err.Error())
-	}
-
-	err = json.Unmarshal(userCfgFile, cfg)
-	if err != nil {
-		return fmt.Errorf("error parsing user config file: %s", err.Error())
-	}
-
-	return nil
+	fmt.Printf("Config %+v \n", service)
 }
---
+++ service/config/reader.go
@@ -0,0 +1,58 @@
+package cservice
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/friendlylinuxplayers/flip.earth/config"
+)
+
+//Reader is the service for getting the application configuration. It cannot be
+// rebuilt or changed after it is first used.
+type Reader struct{}
+
+//Init returns the config service, which is just a struct containing the data.
+func (r Reader) Init(deps map[string]interface{}, conf map[string]interface{}) (interface{}, error) {
+	return parseConfig()
+}
+
+//Reads the default and user json config files, returning a Config.
+func parseConfig() (*config.Config, error) {
+	cfgFile, err := ioutil.ReadFile("config/default/config.json")
+	if err != nil {
+		return nil, fmt.Errorf("error reading default config file: %s", err.Error())
+	}
+	cfg := new(config.Config)
+	err = json.Unmarshal(cfgFile, cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing default config file: %s", err.Error())
+	}
+
+	err = mergeUserConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+//Merges the user config.json data with the provided Config.
+func mergeUserConfig(cfg *config.Config) error {
+	if _, err := os.Stat("config/config.json"); os.IsNotExist(err) {
+		return nil
+	}
+
+	userCfgFile, err := ioutil.ReadFile("config/config.json")
+	if err != nil {
+		return fmt.Errorf("error reading user config file: %s", err.Error())
+	}
+
+	err = json.Unmarshal(userCfgFile, cfg)
+	if err != nil {
+		return fmt.Errorf("error parsing user config file: %s", err.Error())
+	}
+
+	return nil
+}
