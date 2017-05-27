cc4203fb28c96549a093c96070f9392a78c53207
Stephan Lanfermann <stelanf@gmail.com>
2017-05-27 14:49:38 +0200

    Implemented a very basic service container
    Signed-off-by: Stephan Lanfermann <stelanf@gmail.com>

--- service/builder.go
+++ service/builder.go
@@ -1,14 +1,48 @@
 package service
 
+import "fmt"
+
+//Builder is a simple implementation of ContainerBuilder
 type Builder struct {
 	definitions []Definition
 }
 
+//Insert a new definition into the Builder
 func (b *Builder) Insert(def Definition) {
 	b.definitions = append(b.definitions, def)
 }
 
+//Build builds the container once all definitions have been place in it
+//dependencies need to be inserted in order for now
 func (b *Builder) Build() (Container, error) {
-	// TODO: actually process the definitions slice to create a container
-	return nil, nil
+	numDefs := len(b.definitions)
+	servs := make(map[string]interface{}, numDefs)
+	for _, def := range b.definitions {
+		numDeps := len(def.Dependencies)
+		if numDeps == 0 {
+			service, err := def.Initializer.Init(map[string]interface{}{}, def.Configuration)
+			if err != nil {
+				return nil, err
+			}
+			servs[def.Name] = service
+		} else {
+			deps := make(map[string]interface{}, numDeps)
+			for _, name := range def.Dependencies {
+				dep, ok := servs[name]
+				if !ok {
+					return nil, fmt.Errorf("service: Could not find dependency %q for service %q. Please make sure to insert them in order", name, def.Name)
+				}
+				deps[name] = dep
+			}
+			service, err := def.Initializer.Init(deps, def.Configuration)
+			if err != nil {
+				return nil, err
+			}
+			servs[def.Name] = service
+		}
+	}
+
+	return SimpleContainer{
+		services: servs,
+	}, nil
 }
--- service/simple_container.go
+++ service/simple_container.go
@@ -2,18 +2,21 @@ package service
 
 import "fmt"
 
+//SimpleContainer is a simple implementation of Container
 type SimpleContainer struct {
 	services map[string]interface{}
 }
 
-func (sc *SimpleContainer) Get(name string) (interface{}, error) {
+//Get the service by name, if it doesn't exist, an error is returned
+func (sc SimpleContainer) Get(name string) (interface{}, error) {
 	if s, ok := sc.services[name]; ok {
 		return s, nil
 	}
 	return nil, fmt.Errorf("service: Container does not have service %q", name)
 }
 
-func (sc *SimpleContainer) Has(name string) bool {
+//Has checks if a service under the given name exists within the container
+func (sc SimpleContainer) Has(name string) bool {
 	_, ok := sc.services[name]
 	return ok
 }
