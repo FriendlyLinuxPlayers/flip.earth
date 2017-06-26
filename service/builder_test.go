b8cf1c9ba2cca0720ba6608a7db64efd7bffa1a9
Andrew Conrad <aconrad103@gmail.com>
2017-06-26 13:55:39 -0500

    Add some initial tests for service package

---
+++ service/builder_test.go
@@ -0,0 +1,105 @@
+package service
+
+import "testing"
+
+func fakeInit(deps map[string]interface{}, conf map[string]interface{}) (interface{}, error) {
+	return "fakeInit", nil
+}
+func fakeDep(deps map[string]interface{}, conf map[string]interface{}) (interface{}, error) {
+	return "fakeDep", nil
+}
+
+func TestBuilder(t *testing.T) {
+	emptyDef := Definition{}
+	dependencyDef := Definition{
+		Name:          "dep service",
+		Configuration: map[string]interface{}{"config": 1},
+		Init:          fakeDep,
+	}
+	fullDef := Definition{
+		Name:          "service",
+		Dependencies:  []string{"dep service"},
+		Configuration: map[string]interface{}{"config": 1},
+		Init:          fakeInit,
+	}
+	wrongDepsErrorMsg := "service: Could not find dependency \"dep service\" for service \"service\". Please make sure to insert them in order"
+	// TODO add error checking function to Builder so that this isn't needed
+
+	t.Run("Insert1", func(t *testing.T) {
+		b := Builder{}
+		b.Insert(emptyDef)
+		if len(b.definitions) != 1 {
+			t.Errorf("Incorrect number of definitions")
+		}
+	})
+	t.Run("Insert2", func(t *testing.T) {
+		b := Builder{}
+		b.Insert(emptyDef)
+		b.Insert(emptyDef)
+		if len(b.definitions) != 2 {
+			t.Errorf("Incorrect number of definitions")
+		}
+	})
+	// TODO Test every possible combination of fields initialized in a Definition.
+	t.Run("BuildEmpty", func(t *testing.T) {
+		b := Builder{}
+		_, err := b.Build()
+		if err != nil {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+	})
+	t.Run("BuildUninitDef", func(t *testing.T) {
+		// This causes a panic right now. Need to adjust the tests to only
+		// add Definitions that would be valid (as they would be through
+		// Insert).
+		b := Builder{
+			definitions: []Definition{emptyDef},
+		}
+		_, err := b.Build()
+		if err != nil {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+	})
+	t.Run("BuildInitDef1", func(t *testing.T) {
+		b := Builder{
+			definitions: []Definition{dependencyDef},
+		}
+		_, err := b.Build()
+		if err != nil {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+	})
+	t.Run("BuildInitDef2", func(t *testing.T) {
+		b := Builder{
+			definitions: []Definition{dependencyDef, fullDef},
+		}
+		_, err := b.Build()
+		if err != nil {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+	})
+	t.Run("BuildInitInvalidDef1", func(t *testing.T) {
+		b := Builder{
+			definitions: []Definition{fullDef},
+		}
+		_, err := b.Build()
+		if err == nil {
+			t.Errorf("Did not return an error when there were unmet dependencies (not present).")
+		}
+		if err.Error() != wrongDepsErrorMsg {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+	})
+	t.Run("BuildInitInvalidDef2", func(t *testing.T) {
+		b := Builder{
+			definitions: []Definition{fullDef, dependencyDef},
+		}
+		_, err := b.Build()
+		if err == nil {
+			t.Errorf("Did not return an error when there were unmet dependencies (wrong order).")
+		}
+		if err.Error() != wrongDepsErrorMsg {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+	})
+}
---
+++ service/simple_container_test.go
@@ -0,0 +1,41 @@
+package service
+
+import "testing"
+
+func TestSimpleContainer(t *testing.T) {
+	serviceMap := map[string]interface{}{
+		"service": 1,
+	}
+	sc := SimpleContainer{
+		services: serviceMap,
+	}
+
+	t.Run("GetValid", func(t *testing.T) {
+		s, err := sc.Get("service")
+		if err != nil {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+		if s != serviceMap["service"] {
+			t.Errorf("Returned service has the wrong value.")
+		}
+	})
+	t.Run("GetInvalid", func(t *testing.T) {
+		s, err := sc.Get("not a service")
+		if err == nil {
+			t.Errorf("Unexpectedly returned a service: %s", s)
+		}
+		if !IsNotPresent(err) {
+			t.Errorf("Encountered an error: " + err.Error())
+		}
+	})
+	t.Run("HasValid", func(t *testing.T) {
+		if !sc.Has("service") {
+			t.Errorf("Does not have an expected service.")
+		}
+	})
+	t.Run("HasInvalid", func(t *testing.T) {
+		if sc.Has("not a service") {
+			t.Errorf("Unexpectedly has a service.")
+		}
+	})
+}
