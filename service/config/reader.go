a3091023d720c44399b421cc67cd73feff03b08f
Andrew Conrad <aconrad103@gmail.com>
2017-07-07 21:35:55 -0500

    service.config: Support prefix for relative paths

--- service/config/reader.go
+++ service/config/reader.go
@@ -12,31 +12,62 @@ import (
 // Init returns the Config service, a struct containing the parsed data.
 //
 // If a key "config_file" is present in conf, its value will be used as a custom
-// path to the user-provided configuration file. A value of "" (an empty string)
-// indicates that only the default configuration should be used. If the value is
-// invalid, an error is returned. Otherwise, the user config path defaults to
-// "config/config.json"
+// path to the user-provided configuration file. A value of "config/config.json"
+// indicates to search in the default location for a user config file. A value
+// of "" (an empty string) indicates that only the default configuration should
+// be used. If the value is invalid, an error is returned.
+//
+// User-provided configuration files will be merged with the default config file
+// at "config/default/config.json" to set values which are missing.
+//
+// If a key "working_dir" is present in conf, its value will be used as a
+// prefix to all relative paths used in locating configuration files. A value of
+// "" indicates that relative paths will remain unchanged, otherwise it must be
+// a valid directory.
 func Init(deps, conf map[string]interface{}) (interface{}, error) {
-	configVal, ok := conf["config_file"]
+	configPath, ok := valToString(conf["config_file"], "config/config.json")
 	if !ok {
-		return parseConfig("config/config.json")
+		return nil, fmt.Errorf("user provided config_file path is invalid: string type assertion failed")
 	}
-	configPath, ok := configVal.(string)
+	prefixDir, ok := valToString(conf["working_dir"], "")
 	if !ok {
-		return nil, fmt.Errorf("user provided config file path is invalid: string type assertion failed")
+		return nil, fmt.Errorf("user provided working_dir path is invalid: string type assertion failed")
 	}
-	if configPath == "" {
-		return parseConfig("")
+	if prefixDir != "" {
+		dirInfo, err := os.Stat(prefixDir)
+		if os.IsNotExist(err) {
+			return nil, fmt.Errorf("user provided working dir does not exist: %s", prefixDir)
+		}
+		if !dirInfo.IsDir() {
+			return nil, fmt.Errorf("user provided working dir is not a directory: %s", prefixDir)
+		}
 	}
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		return nil, fmt.Errorf("user provided config file does not exist: %s", configPath)
+	if configPath != "" && configPath != "config/config.json" {
+		if _, err := os.Stat(configPath); os.IsNotExist(err) {
+			return nil, fmt.Errorf("user provided config file does not exist: %s", configPath)
+		}
+		// If configPath is relative, append it to prefixDir
+		if !os.IsPathSeparator(configPath[0]) {
+			configPath = prefixDir + configPath
+		}
+
 	}
-	return parseConfig(configPath)
+	return parseConfig(configPath, prefixDir)
+}
+
+// valToString takes an interface and converts it to a string. If value is nil,
+// the provided def is returned.
+func valToString(value interface{}, def string) (string, bool) {
+	if value == nil {
+		return def, true
+	}
+	sVal, ok := value.(string)
+	return sVal, ok
 }
 
 // parseConfig reads the default and user json config files, returning a Config.
-func parseConfig(userConfig string) (*config.Config, error) {
-	cfgFile, err := ioutil.ReadFile("config/default/config.json")
+func parseConfig(userConfig, relPrefix string) (*config.Config, error) {
+	cfgFile, err := ioutil.ReadFile(relPrefix + "config/default/config.json")
 	if err != nil {
 		return nil, fmt.Errorf("error reading default config file: %s", err.Error())
 	}
